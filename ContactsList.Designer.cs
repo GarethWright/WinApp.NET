7c7e5899067bbdd5c013b5bffef91258f1c074bb
Max Kovaljov <shirioko@gmail.com>
2013-05-04 03:08:56 +0200

    Added Import Google button for manual import

--- ContactsList.Designer.cs
+++ ContactsList.Designer.cs
@@ -32,24 +32,25 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnGoogle = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.Enabled = false;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(13, 39);
+            this.listBox1.Location = new System.Drawing.Point(13, 65);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(259, 368);
+            this.listBox1.Size = new System.Drawing.Size(259, 342);
             this.listBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(250, 10);
+            this.button1.Location = new System.Drawing.Point(223, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(22, 23);
+            this.button1.Size = new System.Drawing.Size(49, 49);
             this.button1.TabIndex = 1;
-            this.button1.Text = "+";
+            this.button1.Text = "New chat";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -62,19 +63,31 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Updating contacts...";
             // 
+            // btnGoogle
+            // 
+            this.btnGoogle.Location = new System.Drawing.Point(163, 10);
+            this.btnGoogle.Name = "btnGoogle";
+            this.btnGoogle.Size = new System.Drawing.Size(54, 49);
+            this.btnGoogle.TabIndex = 3;
+            this.btnGoogle.Text = "Import Google";
+            this.btnGoogle.UseVisualStyleBackColor = true;
+            this.btnGoogle.Click += new System.EventHandler(this.btnGoogle_Click);
+            // 
             // ContactsList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 428);
+            this.Controls.Add(this.btnGoogle);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "ContactsList";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.Text = "ContactsList";
+            this.Text = "Chats";
             this.Load += new System.EventHandler(this.ContactsList_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -86,5 +99,6 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGoogle;
     }
 }
